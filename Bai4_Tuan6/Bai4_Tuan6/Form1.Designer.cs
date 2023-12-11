ffd5d15651d9d953817ef70d9bb8795d46cedc13
MarkDev2002 <93862801+MarkDev2002@users.noreply.github.com>
2023-12-11 10:16:49 +0700

    Add files via upload

---
+++ Bai4_Tuan6/Bai4_Tuan6/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Bai4_Tuan6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblInputSoNguyen = new Label();
+            txtSoNguyen = new TextBox();
+            btCapnhat = new Button();
+            lstKetqua = new ListBox();
+            txtListFunc = new TextBox();
+            btTang2 = new Button();
+            btChonchandau = new Button();
+            btLecuoi = new Button();
+            btXoaphantudangchon = new Button();
+            btXoaphantudau = new Button();
+            btXoaphantucuoi = new Button();
+            btKetthuc = new Button();
+            btnReset = new Button();
+            errSoNguyen = new ErrorProvider(components);
+            btnLoad = new Button();
+            btnSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)errSoNguyen).BeginInit();
+            SuspendLayout();
+            // 
+            // lblInputSoNguyen
+            // 
+            lblInputSoNguyen.AutoSize = true;
+            lblInputSoNguyen.Location = new Point(44, 39);
+            lblInputSoNguyen.Name = "lblInputSoNguyen";
+            lblInputSoNguyen.Size = new Size(116, 20);
+            lblInputSoNguyen.TabIndex = 0;
+            lblInputSoNguyen.Text = "Nhập số nguyên";
+            // 
+            // txtSoNguyen
+            // 
+            txtSoNguyen.Location = new Point(201, 36);
+            txtSoNguyen.Name = "txtSoNguyen";
+            txtSoNguyen.Size = new Size(251, 27);
+            txtSoNguyen.TabIndex = 1;
+            // 
+            // btCapnhat
+            // 
+            btCapnhat.Location = new Point(489, 35);
+            btCapnhat.Name = "btCapnhat";
+            btCapnhat.Size = new Size(94, 29);
+            btCapnhat.TabIndex = 2;
+            btCapnhat.Text = "Cập Nhật";
+            btCapnhat.UseVisualStyleBackColor = true;
+            btCapnhat.Click += btCapnhat_Click;
+            // 
+            // lstKetqua
+            // 
+            lstKetqua.FormattingEnabled = true;
+            lstKetqua.ItemHeight = 20;
+            lstKetqua.Location = new Point(54, 84);
+            lstKetqua.Name = "lstKetqua";
+            lstKetqua.Size = new Size(223, 404);
+            lstKetqua.TabIndex = 3;
+            // 
+            // txtListFunc
+            // 
+            txtListFunc.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            txtListFunc.Location = new Point(371, 84);
+            txtListFunc.Multiline = true;
+            txtListFunc.Name = "txtListFunc";
+            txtListFunc.Size = new Size(212, 404);
+            txtListFunc.TabIndex = 4;
+            txtListFunc.Text = "Chức năng";
+            txtListFunc.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btTang2
+            // 
+            btTang2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btTang2.Location = new Point(383, 137);
+            btTang2.Name = "btTang2";
+            btTang2.Size = new Size(188, 29);
+            btTang2.TabIndex = 5;
+            btTang2.Text = "Tăng mỗi phần tử lên 2";
+            btTang2.UseVisualStyleBackColor = true;
+            btTang2.Click += btTang2_Click;
+            // 
+            // btChonchandau
+            // 
+            btChonchandau.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btChonchandau.Location = new Point(383, 193);
+            btChonchandau.Name = "btChonchandau";
+            btChonchandau.Size = new Size(188, 29);
+            btChonchandau.TabIndex = 6;
+            btChonchandau.Text = "Chọn số chẵn đầu";
+            btChonchandau.UseVisualStyleBackColor = true;
+            btChonchandau.Click += btChonchandau_Click;
+            // 
+            // btLecuoi
+            // 
+            btLecuoi.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btLecuoi.Location = new Point(383, 252);
+            btLecuoi.Name = "btLecuoi";
+            btLecuoi.Size = new Size(188, 29);
+            btLecuoi.TabIndex = 7;
+            btLecuoi.Text = "Chọn số lẻ cuối";
+            btLecuoi.UseVisualStyleBackColor = true;
+            btLecuoi.Click += btLecuoi_Click;
+            // 
+            // btXoaphantudangchon
+            // 
+            btXoaphantudangchon.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btXoaphantudangchon.Location = new Point(383, 311);
+            btXoaphantudangchon.Name = "btXoaphantudangchon";
+            btXoaphantudangchon.Size = new Size(188, 29);
+            btXoaphantudangchon.TabIndex = 8;
+            btXoaphantudangchon.Text = "Xoá phần tử đang chọn";
+            btXoaphantudangchon.UseVisualStyleBackColor = true;
+            btXoaphantudangchon.Click += btXoaphantudangchon_Click;
+            // 
+            // btXoaphantudau
+            // 
+            btXoaphantudau.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btXoaphantudau.Location = new Point(383, 371);
+            btXoaphantudau.Name = "btXoaphantudau";
+            btXoaphantudau.Size = new Size(188, 29);
+            btXoaphantudau.TabIndex = 9;
+            btXoaphantudau.Text = "Xoá phần tử đầu";
+            btXoaphantudau.UseVisualStyleBackColor = true;
+            btXoaphantudau.Click += btXoaphantudau_Click;
+            // 
+            // btXoaphantucuoi
+            // 
+            btXoaphantucuoi.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btXoaphantucuoi.Location = new Point(383, 431);
+            btXoaphantucuoi.Name = "btXoaphantucuoi";
+            btXoaphantucuoi.Size = new Size(188, 29);
+            btXoaphantucuoi.TabIndex = 10;
+            btXoaphantucuoi.Text = "Xoá phần tử cuối";
+            btXoaphantucuoi.UseVisualStyleBackColor = true;
+            btXoaphantucuoi.Click += btXoaphantucuoi_Click;
+            // 
+            // btKetthuc
+            // 
+            btKetthuc.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btKetthuc.Location = new Point(54, 550);
+            btKetthuc.Name = "btKetthuc";
+            btKetthuc.Size = new Size(529, 29);
+            btKetthuc.TabIndex = 11;
+            btKetthuc.Text = "Kết thúc";
+            btKetthuc.UseVisualStyleBackColor = true;
+            btKetthuc.Click += btKetthuc_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnReset.Location = new Point(54, 505);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(529, 29);
+            btnReset.TabIndex = 12;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // errSoNguyen
+            // 
+            errSoNguyen.ContainerControl = this;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLoad.Location = new Point(54, 649);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(529, 29);
+            btnLoad.TabIndex = 13;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSave.Location = new Point(54, 604);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(529, 29);
+            btnSave.TabIndex = 14;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(621, 705);
+            Controls.Add(btnSave);
+            Controls.Add(btnLoad);
+            Controls.Add(btnReset);
+            Controls.Add(btKetthuc);
+            Controls.Add(btXoaphantucuoi);
+            Controls.Add(btXoaphantudau);
+            Controls.Add(btXoaphantudangchon);
+            Controls.Add(btLecuoi);
+            Controls.Add(btChonchandau);
+            Controls.Add(btTang2);
+            Controls.Add(txtListFunc);
+            Controls.Add(lstKetqua);
+            Controls.Add(btCapnhat);
+            Controls.Add(txtSoNguyen);
+            Controls.Add(lblInputSoNguyen);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errSoNguyen).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblInputSoNguyen;
+        private TextBox txtSoNguyen;
+        private Button btCapnhat;
+        private ListBox lstKetqua;
+        private TextBox txtListFunc;
+        private Button btTang2;
+        private Button btChonchandau;
+        private Button btLecuoi;
+        private Button btXoaphantudangchon;
+        private Button btXoaphantudau;
+        private Button btXoaphantucuoi;
+        private Button btKetthuc;
+        private Button btnReset;
+        private ErrorProvider errSoNguyen;
+        private Button btnSave;
+        private Button btnLoad;
+    }
+}
---
+++ Bai4_Tuan6/Bai4_Tuan6/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Bai4_Tuan6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCapnhat_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSoNguyen.Text))
+            {
+                MessageBox.Show("Không để trống", "Thông báo");
+            }
+            else if ((!Int32.TryParse(txtSoNguyen.Text, out _)) || Int32.Parse(txtSoNguyen.Text) < 0)
+            {
+                MessageBox.Show("Nhập số nguyên dương!!!", "Thông báo");
+            }
+            else
+            {
+                lstKetqua.Items.Add(txtSoNguyen.Text);
+                txtSoNguyen.Text = "";
+                txtSoNguyen.Focus();
+            }
+        }
+
+        private void btTang2_Click(object sender, EventArgs e)
+        {
+            int n = lstKetqua.Items.Count;
+            for (int i = 0; i < n; ++i)
+            {
+                int tam = int.Parse(lstKetqua.Items[i].ToString()) + 2;
+                lstKetqua.Items.RemoveAt(i);
+                lstKetqua.Items.Insert(i, tam.ToString());
+
+            }
+        }
+
+        private void btChonchandau_Click(object sender, EventArgs e)
+        {
+            int n = lstKetqua.Items.Count;
+            for (int i = 0; i < n - 1; ++i)
+            {
+                if (int.Parse(lstKetqua.Items[i].ToString()) % 2 == 0)
+                {
+                    string s = lstKetqua.Items[i].ToString();
+                    MessageBox.Show("Số chẵn đầu là : " + s, "Kết quả");
+                    break;
+                }
+            }
+
+        }
+
+        private void btLecuoi_Click(object sender, EventArgs e)
+        {
+            int n = lstKetqua.Items.Count;
+            for (int i = n - 1; i >= 0; --i)
+            {
+                if (int.Parse(lstKetqua.Items[i].ToString()) % 2 == 1)
+                {
+                    string s = lstKetqua.Items[i].ToString();
+                    MessageBox.Show("Số lẻ cuối là : " + s, "Kết quả");
+                    break;
+                }
+            }
+
+        }
+
+        private void btXoaphantudangchon_Click(object sender, EventArgs e)
+        {
+            int n = lstKetqua.SelectedItems.Count;
+            for (int i = n - 1; i >= 0; --i)
+            {
+                lstKetqua.Items.Remove(lstKetqua.SelectedItems[i].ToString());
+            }
+        }
+
+        private void btXoaphantudau_Click(object sender, EventArgs e)
+        {
+            lstKetqua.Items.Remove(lstKetqua.Items[0].ToString());
+        }
+
+        private void btXoaphantucuoi_Click(object sender, EventArgs e)
+        {
+            int n = lstKetqua.Items.Count;
+            lstKetqua.Items.Remove(lstKetqua.Items[n - 1].ToString());
+        }
+
+        private void btKetthuc_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtSoNguyen.Text = "";
+            lstKetqua.Items.Clear();
+        }
+        public class Customer
+        {
+            
+
+            public Customer(string name, int cost)
+            {
+                this.name = name;
+                this.cost = cost;
+            }
+            public string Name { set { name = value; } get { return name; } }
+            public int Cost { get { return cost; } set { cost = value; } }
+            public override string ToString()
+            {
+                return $"{Name} - {Cost}";
+            }
+        }
+        string filePath = "C:\\Users\\DELL\\source\\repos\\Bai1_2_3\\Bai1_2_3\\files\\";
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Tệp văn bản (*.txt)|*.txt|Tất cả các tệp (*.*)|*.*";
+            //if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath + "test.txt"))
+                {
+                    foreach (Customer item in listBox1.Items)
+                    {
+                        writer.Write($"{item.Name},{item.Cost}\n");
+                    }
+                }
+
+                MessageBox.Show("Danh sách đã được lưu vào tệp thành công.", "Thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi lưu tệp: " + ex.Message, "Lỗi");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Tệp văn bản (*.txt)|*.txt|Tất cả các tệp (*.*)|*.*";
+            //if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            try
+            {
+                listBox1.Items.Clear();
+                string fileContent = File.ReadAllText(filePath + "test.txt");
+                string[] splitContent = fileContent.Split('\n');
+                for (int i = 0; i < splitContent.Length - 1; i++)
+                {
+                    string[] getObj = splitContent[i].Split(",");
+                    Customer customer = new Customer(getObj[0], int.Parse(getObj[1]));
+                    listBox1.Items.Add(customer);
+                }
+                listBox1.Text = fileContent;
+
+                MessageBox.Show("Đọc tệp thành công.", "Thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đọc tệp: " + ex.Message, "Lỗi");
+            }
+        }
+    }
+}
